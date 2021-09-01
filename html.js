303bcdbc839bcf5dc2da2c58e3b008f2d638252d
Kaiying Fu <kaiying.fu@gmail.com>
2021-09-01 10:31:27 +0800

    feat: Add simple HTTP server utilising HTML file

---
+++ html.js
@@ -0,0 +1,28 @@
+// Sample server that serves direct HTML
+const http = require("http");
+const fs = require('fs').promises;
+
+let indexFile;
+
+const host = '0.0.0.0';
+const port = 8000;
+
+const requestListener = function (req, res) {
+  res.setHeader("Content-Type", "text/html");
+  res.writeHead(200);
+  res.end(indexFile);
+};
+
+const server = http.createServer(requestListener);
+
+fs.readFile(__dirname + "/index.html")
+  .then(contents => {
+      indexFile = contents;
+      server.listen(port, host, () => {
+          console.log(`Server is running on http://${host}:${port}`);
+      });
+  })
+  .catch(err => {
+      console.error(`Could not read index.html file: ${err}`);
+      process.exit(1);
+  });
