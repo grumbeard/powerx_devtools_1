8c5225b1441bde75bfd6c57c480514173106855d
Kaiying Fu <kaiying.fu@gmail.com>
2021-09-01 09:59:12 +0800

    feat: Add simple HTTP server to repo

---
+++ server.js
@@ -0,0 +1,20 @@
+const http = require("http");
+
+const host = '0.0.0.0';
+const port = 8000;
+
+// const requestListener = function (req, res) {
+//     res.writeHead(200);
+//     res.end("My first server!");
+// };
+
+const requestListener = function (req, res) {
+  res.setHeader("Content-Type", "text/html");
+  res.writeHead(200);
+  res.end("<b>Hello</b>");
+};
+
+const server = http.createServer(requestListener);
+server.listen(port, host, () => {
+    console.log(`Server is running on http://${host}:${port}`);
+});
